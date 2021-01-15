caa05a27983ce4633a4da941d2c43d52ac994393
sywvc24 <75411125+sywvc24@users.noreply.github.com>
2021-01-15 23:28:07 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ const tl = gsap.timeline();
 tl.to('.slideshow', {
         opacity: 1,
         duration: 1,
-        ease: 'power2.out'
+        ease: 'power1.out'
     })
     .set('.catch-copy', {
-        className: "+=reveal-text"
+        className: '+=reveal-text'
     })
     .to('.scroll-down', {
         opacity: 1,
         duration: 1,
-        ease: 'power2.out'
+        ease: 'power1.out'
     });
