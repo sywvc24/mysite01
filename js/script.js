3bca1172c53d5f3d6464291446e18c6ff99521b0
sywvc24 <75411125+sywvc24@users.noreply.github.com>
2021-01-15 20:11:24 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const tl = gsap.timeline();
+
+tl.to('.slideshow', {
+        opacity: 1,
+        duration: 1,
+        ease: 'power2.out'
+    })
+    .set('.catch-copy', {
+        className: "+=reveal-text"
+    })
+    .to('.scroll-down', {
+        opacity: 1,
+        duration: 1,
+        ease: 'power2.out'
+    });
