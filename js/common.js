03ff6107103de7b7c946687e2bb03ed0dfe5465d
sywvc24 <75411125+sywvc24@users.noreply.github.com>
2021-01-15 20:10:32 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -58,24 +58,3 @@ if (touch) { // remove all :hover stylesheets
         }
     } catch (ex) {}
 }
-
-
-/**
- * animation
- *
- */
-const tl = gsap.timeline();
-
-tl.to('.slideshow', {
-        opacity: 1,
-        duration: 1,
-        ease: 'power2.out'
-    })
-    .set('.catch-copy', {
-        className: "+=reveal-text"
-    })
-    .to('.scroll-down', {
-        opacity: 1,
-        duration: 1,
-        ease: 'power2.out'
-    });
