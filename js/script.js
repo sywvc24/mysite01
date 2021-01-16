7fd20db3afda957a4034c5951b0e4b481e8388d4
sywvc24 <75411125+sywvc24@users.noreply.github.com>
2021-01-17 00:03:52 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ window.addEventListener('DOMContentLoaded', () => {
         })
         .to('.slideshow', {
             opacity: 1,
-            duration: 1,
+            duration: 0.8,
             ease: 'power2.out'
         })
         .set('.catch-copy', {
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', () => {
         })
         .to('.scroll-down', {
             opacity: 1,
-            duration: 1,
+            duration: 0.8,
             ease: 'power2.out'
         })
 });
