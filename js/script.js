5ed7d2e5b65a3348c617a7e8fc310876f8f7c3f0
sywvc24 <75411125+sywvc24@users.noreply.github.com>
2021-01-18 00:53:52 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 const tl = gsap.timeline();
-
-tl.to('.h-inner', {
-        opacity: 1,
-        duration: 0.5,
-        ease: 'power2.out'
-    })
-    .to('.slide-block', {
-        width: 0,
-        duration: 0.8,
-        ease: 'power2.out'
-    })
-    .set('.catch-copy', {
-        className: '+=reveal-text'
-    })
-    .to('.scroll-down', {
-        opacity: 1,
-        duration: 0.8,
-        ease: 'power2.out'
-    });
+window.addEventListener('load', () => {
+    tl.to('.h-inner', {
+            opacity: 1,
+            duration: 0.5,
+            ease: 'power2.out'
+        })
+        .to('.slideshow', {
+            opacity: 1,
+            duration: 0.8,
+            ease: 'power2.out'
+        })
+        .set('.catch-copy', {
+            className: '+=reveal-text'
+        })
+        .to('.scroll-down', {
+            opacity: 1,
+            duration: 0.8,
+            ease: 'power2.out'
+        })
+});
