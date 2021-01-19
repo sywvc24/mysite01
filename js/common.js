3b12e0af731098bf309199da629759910eca73a2
sywvc24 <75411125+sywvc24@users.noreply.github.com>
2021-01-19 16:23:14 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -83,3 +83,34 @@ window.addEventListener('load', () => {
         })
 });
 
+/**
+ * Intersection Observer
+ * scroll-animation
+ */
+const iSObservers = document.querySelectorAll('.waypoint');
+const options = {
+    threshold: 0.3
+};
+
+const inView = (target) => {
+    if (target.classList.contains('img-anim')) {
+        target.classList.add('img-move');
+    } else if (target.classList.contains('ttl-anim')) {
+        target.classList.add('movingLetter');
+    } else if (target.classList.contains('cont-img')) {
+        target.classList.add('fadeIn');
+    }
+};
+
+const observeUse = (entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            inView(entry.target);
+        }
+    });
+};
+
+const observer = new IntersectionObserver(observeUse, options);
+iSObservers.forEach(iSObserver => {
+    observer.observe(iSObserver);
+});
