8edff0aacf6c8d0e168f6e6a59d82b6e2bce6b2f
sywvc24 <75411125+sywvc24@users.noreply.github.com>
2021-02-26 19:39:14 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -108,8 +108,3 @@ const observer = new IntersectionObserver(observeUse, options);
 iSObservers.forEach(iSObserver => {
     observer.observe(iSObserver);
 });
-
-window.addEventListener("scroll", function () {
-    const header = document.querySelector("header");
-    header.classList.toggle("scroll-nav", window.scrollY > 0);
-});
